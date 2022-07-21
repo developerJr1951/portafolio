45c146d7186b6bb325c72e1e454b5d04c29fe516
Brayan1951 <brayanc1951@gmail.com>
2022-07-20 20:31:50 -0400

    footer fixed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { About } from "../components/about/About";
-import { Footer } from "../components/footer/footer";
+import { Footer } from "../components/footer/Footer";
 import { Hobies } from "../components/hobbies/hobies";
 import { Presentation } from "../components/presentation/Presentation";
 import { Skills } from "../components/skills/Skills";

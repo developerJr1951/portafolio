47f7af628e877152797d5bb1dc443a4cba22469b
developerJr1951 <105681237+developerJr1951@users.noreply.github.com>
2022-07-20 20:37:33 -0400

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { About } from "../components/about/About";
-import { Footer } from "../components/footer/footer";
-import { Hobies } from "../components/hobbies/hobies";
+import { Footer } from "../components/footer/Footer";
+import { Hobies } from "../components/hobbies/Hobies";
 import { Presentation } from "../components/presentation/Presentation";
 import { Skills } from "../components/skills/Skills";
 
